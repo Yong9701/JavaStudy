9d2e99d890d02df01d83758fd46157df73d7f28c
Yong9701 <yong91101@gmail.com>
2024-05-30 17:57:11 +0900

    24년5월30일 Java강의

--- src/ex05method/E06LocalVariable.java
+++ src/ex05method/E06LocalVariable.java
@@ -19,6 +19,11 @@ public class E06LocalVariable {
 		 */
 		// 이 부분을 주석처리하지 않으면 아래 코드에서는 에러가 발생한다.
 //		int num = 9;
+		
+		/*
+		 main보다 좁은 지역인 if문의 블럭을 생성한 후 변수 num을 선언한다.
+		 단 if문과 else문은 서로 다른 지역이므로 동일한 이름의 변수를 선언할 수 있다.
+		 */
 		if(scope) {
 			int num = 1;
 			num++;
@@ -28,6 +33,9 @@ public class E06LocalVariable {
 			int num = 5;
 			System.out.println("첫번째 if문의 else 지역: "+ num);
 		}
+		/*
+		 if문의 블럭에서 선언된 변수 num은 해당 블럭을 벗어나는 순간 메모리에서 소멸되므로 아래에서 num을 새롭게 선언할 수 있다.
+		 */
 		int num = 100;
 		System.out.println("main메소드지역: "+ num);
 		
@@ -35,6 +43,11 @@ public class E06LocalVariable {
 		
 		System.out.println("main메소드 끝");
 	}
+	
+	/*
+	 main지역에서 선언한 변수 num은 메모리에 아직 상주하고 있지만 해당 메서드는 크기가 동일한 다른 지역에 정의되었으므로 같은 이름의 변수를 선언할 수 있다.
+	 앞의 main메서드의 if~else문의 블럭도 같은 개념으로 변수를 선언한 것이다.
+	 */
 	static void simpleFunc() {
 		int num = 1000;
 		System.out.println("simple메소드 지역: "+ num);
